ab6a567124d0fda8a2f6590b89e0367f919e2a2c
acebyte <theacebyte@gmail.com>
2026-02-25 01:08:41 +0000

    Modify sieves.h to support spf

--- include/sieves.h
+++ include/sieves.h
@@ -1,34 +1,43 @@
 #pragma once
 
+#include <cassert>
 #include <cstddef>
 #include <cstdint>
 #include <vector>
 
 using i64 = std::int64_t;
 
-class sieves {
+template <i64 N> class sieves {
 private:
-  std::vector<std::size_t> primes;
+  std::vector<i64> primes;
+  std::vector<i64> spf;
 
 public:
-  sieves(std::size_t n) {
-    std::vector<bool> primality(n + 1, true);
-    primality[0] = primality[1] = false;
-    for (std::size_t i = 0; i <= n; ++i) {
-      if (!primality[i]) {
+  sieves() : spf(N + 1) {
+    std::vector<bool> is_prime(N + 1, true);
+    is_prime[0] = is_prime[1] = false;
+    for (i64 i = 0; i <= N; ++i) {
+      if (!is_prime[i]) {
         continue;
       }
 
       primes.push_back(i);
-      if ((i64)i * i > n) {
+      spf[i] = i;
+      if ((i64)i * i > N) {
         continue;
       }
 
-      for (std::size_t j = i * i; j <= n; j += i) {
-        primality[j] = false;
+      for (i64 j = i * i; j <= N; j += i) {
+        is_prime[j] = false;
+        spf[j] = i;
       }
     }
   }
 
-  const std::vector<std::size_t> &get_primes() const { return primes; }
+  const std::vector<i64> &get_primes() const { return primes; }
+
+  i64 get_spf(i64 x) {
+    assert(x >= 0 && x <= N);
+    return spf[x];
+  }
 };
